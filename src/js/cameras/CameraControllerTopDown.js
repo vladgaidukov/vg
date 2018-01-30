fb8598f75c314c858a41ae0b014411ed7e416c94
Vlad Gaydukov <vladislavgaydukov@gmail.com>
2018-01-30 18:38:18 +0300

    работает topdownshooter

--- src/js/cameras/CameraControllerTopDown.js
+++ src/js/cameras/CameraControllerTopDown.js
@@ -2,45 +2,19 @@ VG.CameraControllerTopDown = function (options) {
 
     options = options || {}
 
-    this.offset = options.offset || new THREE.Vector3(0, 120, 0);
-    this.offsetlook = options.offsetlook || new THREE.Vector3(10, 0, 10);
+    this.offset = options.offset || new THREE.Vector3(0, 120, 120);
     this.target = options.target || new THREE.Mesh();
 
-    var mouse = new THREE.Vector3(0, 0, 0);
-    var newpos = new THREE.Vector3(0, 0, 0);
-    var newlook = new THREE.Vector3(0, 0, 0);
     var camera = options.camera || console.error('options.camera is undefind');
-    var renderer = options.renderer || console.error('options.renderer is undefind');
-
-    this.mousemove = function (evt) {
-        mouse.x = (((evt.x - renderer.domElement.offsetLeft) / renderer.domElement.width) * 2 - 1);
-        mouse.z = (((evt.y - renderer.domElement.offsetTop) / renderer.domElement.height) * 2 - 1);
-    };
 
     this.update = function () {
 
-        newpos.x = this.target.position.x + this.offset.x;
-        newpos.y = this.target.position.y + this.offset.y;
-        newpos.z = this.target.position.z + this.offset.z;
-
-        this.z_offset = mouse.z * this.offsetlook.z;
-        this.x_offset = mouse.x * this.offsetlook.x;
-
-        camera.position.x = newpos.x + this.x_offset
-        camera.position.y = newpos.y;
-        camera.position.z = newpos.z + this.z_offset
-
-        newlook.x = this.target.position.x + this.x_offset;
-        newlook.y = this.target.position.y;
-        newlook.z = this.target.position.z + this.z_offset;
-
-        camera.lookAt(newlook);
+        camera.position.copy(this.target.position.clone().add(this.offset));
+        camera.lookAt(this.target.position);
 
     };
     this.destroy = function () {
-        VG.EventDispatcher.unbind('mouse.move', this.mousemove)
+        return;
     };
-
-    VG.EventDispatcher.bind('mouse.move', this, this.mousemove)
 
 };
